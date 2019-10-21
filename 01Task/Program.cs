11926f0483c5f4fc746d000335f6257411335037
kristiyan8807 <kristiyan8807@gmail.com>
2019-10-21 10:47:55 +0300

    Chapter8

---
+++ 01Task/Program.cs
@@ -0,0 +1,17 @@
+﻿//Превърнете числата 151, 35, 43, 251, 1023 и 1024 в двоична
+//бройна система.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("151 to binary {0}.", Convert.ToString(151, 2));
+        Console.WriteLine("35 to binary {0}.", Convert.ToString(35, 2));
+        Console.WriteLine("43 to binary {0}.", Convert.ToString(43, 2));
+        Console.WriteLine("251 to binary {0}.", Convert.ToString(251, 2));
+        Console.WriteLine("1023 to binary {0}.", Convert.ToString(1023, 2));
+        Console.WriteLine("1024 to binary {0}.", Convert.ToString(1024, 2));
+
+    }
+}
---
+++ 02Task/Program.cs
@@ -0,0 +1,13 @@
+﻿//Превърнете числото 1111010110011110(2) в шестнадесетична и в десе-
+//тична бройна система.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("1111010110011110 to Hex is: {0} ", Convert.ToInt64("1111010110011110", 2).ToString("X"));
+        Console.WriteLine("1111010110011110 to Decimal is {0}.", Convert.ToInt32("1111010110011110", 2));
+
+    }
+}
